f59f6e7f2c23848f2c24fd706502b396a1c3d79d
Yogesh Sonawane <yogeshsonawane@Mac.lan>
2026-04-09 13:49:51 +0530

    api health end point added

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ APPLICATIONS = {
         "deploy_path": BASE_DEPLOY_DIR / "famvest",
         "symlink": "famvest.jar",
         "application_url": "https://famvest.upvaly.com",
-        "api_url": "https://api.famvest.upvaly.com/health"
+        "api_health_end_point": "https://api.famvest.upvaly.com/health"
     },
     "netly": {
         "git_url": "git@github.com/yogeshsonawane-dev/netly.git",
